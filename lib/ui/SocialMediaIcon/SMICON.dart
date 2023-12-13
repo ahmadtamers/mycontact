fa373d1360b8f4ed5cfe7488d6fc0b138f1bd002
Ahmad_Tamer <ahmadtamerats@gmail.com>
2023-12-13 23:51:49 +0000

    028-Lifting State Up

--- lib/ui/SocialMediaIcon/SMICON.dart
+++ lib/ui/SocialMediaIcon/SMICON.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+String? PlatformIconHistory;
+Uri? PlatformLinkHistory;
+
 class SMediaIcon extends StatelessWidget {
   String iconAdd;
   String iconURL;
+  Function HistoryIconState;
   SMediaIcon({
     Key? key,
     required this.iconAdd,
     required this.iconURL,
+    required this.HistoryIconState,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
+        PlatformIconHistory = iconAdd;
+        PlatformLinkHistory = Uri.parse(iconURL);
+        HistoryIconState();
         launchUrl(Uri.parse(iconURL), mode: LaunchMode.externalApplication);
       },
       child: Image(
